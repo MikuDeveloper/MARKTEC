be22f69d8abc7dba8f9f1163eb95d5e93ec7af84
Julio <jcfmalc@gmail.com>
2024-02-03 04:19:10 -0600

    ngOnInit
    agregue el fragmento del OnInit para poder recibir los documentoes de la coleccion de customers y realizar las búsqueda con el Typehead.

--- src/app/sale/sale.component.ts
+++ src/app/sale/sale.component.ts
@@ -23,11 +23,19 @@ export class SaleComponent {
     status:'',
     debt: 0
   }
+  originalCustomers:CustomerModel[] =[]
+  customer:CustomerModel[] =[]
   showCard : boolean = false
   showBtn : boolean = true
   constructor(private navService: NavService,private databaseService: FirestoreService) {
     this.navService.toggleNav(true);
-
+  }
+  async ngOnInit(){
+    // Aquí, estás obteniendo datos de la colección "customers" de tu base de datos.
+    // La función getCollectionDataC() es una función asíncrona, por lo que utilizas 'await' para esperar a que se resuelva antes de continuar.
+    this.originalCustomers = await this.databaseService.getCollectionDataC("customers");
+    // Estás haciendo una copia de los datos originales de los clientes para poder manipularlos sin alterar los datos originales.
+    this.customer = [...this.originalCustomers]
   }
   // Método para recolectar los datos del modal y mostrarlos en el
   addNewCustomer(form:CustomerModel){
