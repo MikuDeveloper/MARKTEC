bb520b7a6ca1af73044a9b53ae1d69f1fa401ca6
Codeminer15 <136392287+Codeminer15@users.noreply.github.com>
2024-02-07 01:05:43 -0600

    Cambios en el amount modal

--- src/app/debts/debts.component.ts
+++ src/app/debts/debts.component.ts
@@ -11,7 +11,23 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './debts.component.scss'
 })
 export class DebtsComponent {
+  defaultDate: string;
+  defaultTime: string;
   constructor(private navService: NavService) {
     this.navService.toggleNav(true);
+    const currentDate = new Date();
+    this.defaultDate = currentDate.toISOString().substring(0, 10); // Formato 'YYYY-MM-DD'
+    this.defaultTime = this.formatTime(currentDate); // Formato 'HH:MM'
+  }
+  private formatTime(date: Date): string {
+    let hours = date.getHours().toString();
+    let minutes = date.getMinutes().toString();
+
+    // Asegurarse de que los minutos tengan dos dígitos
+    if (minutes.length === 1) {
+      minutes = '0' + minutes;
+    }
+
+    return hours + ':' + minutes;
   }
 }
