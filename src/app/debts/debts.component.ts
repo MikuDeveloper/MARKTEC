7f68a774996d6ea6f4259621020239b8ea42d350
Julio <jcfmalc@gmail.com>
2024-02-16 10:41:36 -0600

    get Customers for debts
    so I have make a request get for the customers.

--- src/app/debts/debts.component.ts
+++ src/app/debts/debts.component.ts
@@ -3,12 +3,14 @@ import {NavService} from "../../model/utils/navbar.util";
 import { CommonModule, NgClass } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CustomerModel } from '../../model/entities/customer.model';
+import { FirestoreService } from '../../model/api/firestore.service';
 
 @Component({
   selector: 'app-debts',
   standalone: true,
   imports: [
-    FormsModule, 
+    FormsModule,
     CommonModule,
     NgClass,
     RouterLink,
@@ -18,15 +20,40 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   templateUrl: './debts.component.html',
   styleUrl: './debts.component.scss'
 })
+
+
+
 export class DebtsComponent {
   defaultDate: string;
   defaultTime: string;
-  constructor(private navService: NavService) {
+
+  originalCustomers : CustomerModel [] = []
+  customers : CustomerModel [] = []
+  customer : CustomerModel = {
+    address: '',
+    debt: 0,
+    email: '',
+    name: '',
+    phoneNumber: '',
+    facebook: '',
+    status: '',
+    voterKey: ''
+  }
+  constructor(private navService: NavService, private databaseService:FirestoreService) {
     this.navService.toggleNav(true);
     const currentDate = new Date();
     this.defaultDate = currentDate.toISOString().substring(0, 10); // Formato 'YYYY-MM-DD'
     this.defaultTime = this.formatTime(currentDate); // Formato 'HH:MM'
   }
+
+  async ngOnInit(){
+    // Aquí, estás obteniendo datos de la colección "customers" de tu base de datos.
+    // La función getCollectionDataC() es una función asíncrona, por lo que utilizas 'await' para esperar a que se resuelva antes de continuar.
+    this.originalCustomers = await this.databaseService.getCollectionDataC("customers");
+    // Estás haciendo una copia de los datos originales de los clientes para poder manipularlos sin alterar los datos originales.
+    this.customers = [...this.originalCustomers]
+  }
+
   private formatTime(date: Date): string {
     let hours = date.getHours().toString();
     let minutes = date.getMinutes().toString();
