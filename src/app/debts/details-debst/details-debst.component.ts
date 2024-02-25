146db682b44bdb705564ab3971573cf4ea31a6dd
Codeminer15 <136392287+Codeminer15@users.noreply.github.com>
2024-02-24 23:59:07 -0600

    Modal de Monto

--- src/app/debts/details-debst/details-debst.component.ts
+++ src/app/debts/details-debst/details-debst.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { NavService } from '../../../model/utils/navbar.util';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-details-debst',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [
+    FormsModule,
+    CommonModule,
+    RouterLink, 
+    RouterLinkActive, 
+    RouterOutlet],
   templateUrl: './details-debst.component.html',
   styleUrl: './details-debst.component.scss'
 })
